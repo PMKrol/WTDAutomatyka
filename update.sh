bc68fd775ec3421daa1d2d5240d78bf647985a19
PMKrol <88658026+PMKrol@users.noreply.github.com>
2022-05-12 14:48:27 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-//This script updates EAGLE's library and ulps 
-// and Arduino's library folder
+#This script updates EAGLE's library and ulps 
+# and Arduino's library folder
 
-cd Pobrane
+cd ~/Pobrane
 wget https://github.com/PMKrol/WTDAutomatyka/archive/refs/heads/main.zip -O WTDAutomatyka.zip
 7z x WTDAutomatyka.zip -aoa
 cp -r WTDAutomatyka-main/snap/arduino/current/* /home/student/snap/arduino/current/
