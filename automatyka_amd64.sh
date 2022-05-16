7493c789ea9345ccb7d9567e70b5134fdbbe21bc
PMKrol <88658026+PMKrol@users.noreply.github.com>
2022-05-16 16:35:57 +0200

    Allow to shutdown over ssh without root password

--- automatyka_amd64.sh
+++ automatyka_amd64.sh
@@ -221,11 +221,42 @@ sudo apt update && sudo apt upgrade -y
 
 ### NEW ###
 ### disable password prompt for poweroff ###
-echo "[Shutdown or suspend without a password]
-Identity=unix-user:student
-Action=org.freedesktop.login1.set-wall-message;org.freedesktop.login1.halt;org.freedesktop.login1.suspend
+echo "[Allow all users to shutdown]
+Identity=unix-user:*
+Action=org.freedesktop.login1.power-off-multiple-sessions
 ResultAny=yes
-" | sudo tee /etc/polkit-1/localauthority/50-local.d/allow_shutdown_suspend.pkla
+ResultActive=yes
+
+[Allow all users to reboot]
+Identity=unix-user:*
+Action=org.freedesktop.login1.reboot-multiple-sessions
+ResultAny=yes
+ResultActive=yes
+
+[Allow all users to suspend]
+Identity=unix-user:*
+Action=org.freedesktop.login1.suspend-multiple-sessions
+ResultAny=yes
+ResultActive=yes
+
+[Allow all users to ignore inhibit of shutdown]
+Identity=unix-user:*
+Action=org.freedesktop.login1.power-off-ignore-inhibit
+ResultAny=yes
+ResultActive=yes
+
+[Allow all users to ignore inhibit of reboot]
+Identity=unix-user:*
+Action=org.freedesktop.login1.reboot-ignore-inhibit
+ResultAny=yes
+ResultActive=yes
+
+[Allow all users to ignore inhibit of suspend]
+Identity=unix-user:*
+Action=org.freedesktop.login1.suspend-ignore-inhibit
+ResultAny=yes
+ResultActive=yes
+" | sudo tee /etc/polkit-1/localauthority/50-local.d/nofurtherlogin.pkla
 
 ### hosts
 sudo nano /etc/hostname
