f253f70679bb9e9e78ea7ad4edd693ca8164a078
PMKrol <88658026+PMKrol@users.noreply.github.com>
2022-05-31 12:22:06 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,8 +5,9 @@
 
 cd ~/Pobrane
 wget -q https://github.com/PMKrol/WTDAutomatyka/archive/refs/heads/main.zip -O WTDAutomatyka.zip -q && echo "wget ok"
-7z x WTDAutomatyka.zip -aoa > /dev/null
+7z x WTDAutomatyka.zip -aoa -bso0 -bsp0
 cp -r WTDAutomatyka-main/snap/arduino/current/* /home/student/snap/arduino/current/
 cp -r WTDAutomatyka-main/EAGLE/ /home/student
+chmod 0777 /home/student/Pulpit/Dodatek.txt
 cp WTDAutomatyka-main/Dodatek.ngc /home/student/Pulpit/Dodatek.txt
 chmod 0444 /home/student/Pulpit/Dodatek.txt
