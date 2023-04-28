3ecaa569615b6cc099a0d738ea2ed11209bf9e5a
PMKrol <88658026+PMKrol@users.noreply.github.com>
2023-04-28 16:58:39 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -13,3 +13,36 @@ chmod 0777 /home/student/Pulpit/Dodatek.txt
 rm /home/student/Pulpit/Dodatek.txt
 cp WTDAutomatyka-main/Dodatek.ngc /home/student/Pulpit/Dodatek.txt
 chmod 0444 /home/student/Pulpit/Dodatek.txt
+
+### disable password prompt for poweroff ###
+echo "[Allow all users to shutdown]
+Identity=unix-user:*
+Action=org.freedesktop.login1.power-off-multiple-sessions
+ResultAny=yes
+ResultActive=yes
+[Allow all users to reboot]
+Identity=unix-user:*
+Action=org.freedesktop.login1.reboot-multiple-sessions
+ResultAny=yes
+ResultActive=yes
+[Allow all users to suspend]
+Identity=unix-user:*
+Action=org.freedesktop.login1.suspend-multiple-sessions
+ResultAny=yes
+ResultActive=yes
+[Allow all users to ignore inhibit of shutdown]
+Identity=unix-user:*
+Action=org.freedesktop.login1.power-off-ignore-inhibit
+ResultAny=yes
+ResultActive=yes
+[Allow all users to ignore inhibit of reboot]
+Identity=unix-user:*
+Action=org.freedesktop.login1.reboot-ignore-inhibit
+ResultAny=yes
+ResultActive=yes
+[Allow all users to ignore inhibit of suspend]
+Identity=unix-user:*
+Action=org.freedesktop.login1.suspend-ignore-inhibit
+ResultAny=yes
+ResultActive=yes
+" | sudo tee /etc/polkit-1/localauthority/50-local.d/nofurtherlogin.pkla && echo "OK!"
