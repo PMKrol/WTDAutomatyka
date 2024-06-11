fc9db60dd59136d2cb2b6aa780ead034cf00e0b6
Patryk M Król <88658026+PMKrol@users.noreply.github.com>
2024-06-11 11:38:56 +0200

    Update veyon.sh

--- veyon.sh
+++ veyon.sh
@@ -1,3 +1,4 @@
 sudo apt install veyon-service -y
 sudo systemctl enable veyon.service
 sudo wget "https://raw.githubusercontent.com/PMKrol/WTDAutomatyka/main/Veyon.conf" -O /etc/xdg/Veyon\ Solutions/Veyon.conf
+sudo service veyon restart
