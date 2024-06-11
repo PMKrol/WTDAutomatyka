6b8f737a828190942caa5b530857f73dcfdf5120
Patryk M Król <88658026+PMKrol@users.noreply.github.com>
2024-06-11 11:01:13 +0200

    Add files via upload

---
+++ veyon.sh
@@ -0,0 +1,3 @@
+sudo apt install veyon-service
+sudo systemctl enable veyon.service
+sudo wget "https://raw.githubusercontent.com/PMKrol/WTDAutomatyka/main/Veyon.conf" -O /etc/xdg/Veyon\ Solutions/Veyon.conf
