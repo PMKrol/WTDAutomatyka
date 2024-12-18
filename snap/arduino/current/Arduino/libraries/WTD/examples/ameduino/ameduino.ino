ff71b9137367fb8d02dd3f86fad340daed37c0e2
Patryk M Król <88658026+PMKrol@users.noreply.github.com>
2024-12-18 15:05:00 +0100

    Add files via upload

--- snap/arduino/current/Arduino/libraries/WTD/examples/ameduino/ameduino.ino
+++ snap/arduino/current/Arduino/libraries/WTD/examples/ameduino/ameduino.ino
@@ -1,1 +1,16 @@
+void setup() {
+  ADMUX =  B01100111;           // set internal aref and port A7
+  ADCSRA = B10101100;           // ADC enable, interrupt enable, 16 prescaler
+  ADCSRB = B00000000;           // free running mode
+  sei();            // enable interrupts
+  ADCSRA |=B01000000;           // start first conversion
+  Serial.begin(1000000);
+}
 
+void loop() {
+    delay(10);
+}
+
+ISR(ADC_vect) {
+    Serial.write(ADCH);
+}
